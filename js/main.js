6e8fd50c38768acf7d5f0a17b078fae1d211d98d
Kext <25501036+Anexis712@users.noreply.github.com>
2020-11-17 08:10:25 +0900

    add check browser feature

--- js/main.js
+++ js/main.js
@@ -27,6 +27,22 @@ function auto_scroll(num) {
     });
 }
 
+function onload(){
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0) // If Internet Explorer, return version number
+    {
+        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+    }
+    else  // If another browser, return 0
+    {
+        alert('otherbrowser');
+    }
+
+    return false;
+}
+
 
 const change_language = [
     [
