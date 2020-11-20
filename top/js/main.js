e15007f437a5f194fe8981628d82c11a10d826fd
Kext <25501036+Anexis712@users.noreply.github.com>
2020-11-21 08:00:42 +0900

    consolidated verification scripts

--- top/js/main.js
+++
@@ -1,8 +0,0 @@
-function isLoggedIn(){
-	const token = store.get('token');
-    if (!token){
-		alert('認証コードが必要です。');
-        window.location.replace("../auth");
-    } 
-    document.documentElement.classList.remove('no-js');
-}
